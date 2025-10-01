dd06c6748b70d4ff4a0d1993cca53fd200b99bdb
Carokyp <97530091+Carokyp@users.noreply.github.com>
2025-10-01 17:38:38 +0100

    Refactor game navigation and update button IDs for clarity

--- game.js
+++ game.js
@@ -1,22 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menu = document.getElementById("menu");
-  const howToPlay = document.getElementById("how-to-play-area");
-  const game = document.getElementById("game-area");
+  const howToPlayArea = document.getElementById("how-to-play-area");
+  const gameArea = document.getElementById("game-area");
 
+  // Fonction pour démarrer le jeu
   function startGame() {
     document.getElementById("start").addEventListener("click", () => {
       menu.style.display = "none";
-      game.style.display = "block";
+      gameArea.style.display = "block";
     });
   }
 
-  function howToPlay() {
-    document.getElementById("how-to-play-button").addEventListener("click", () => {
+  function howToPlayButton() {
+    document
+      .getElementById("how-to-play-button")
+      .addEventListener("click", () => {
         menu.style.display = "none";
-        howToPlay.style.display = "block";
+        howToPlayArea.style.display = "flex";
       });
   }
 
+  function backToMenu() {
+    // Bouton back de How to Play
+    document
+      .getElementById("back-to-menu-howto")
+      .addEventListener("click", () => {
+        howToPlayArea.style.display = "none";
+        menu.style.display = "flex";
+      });
+
+    // Bouton back de Game Area
+    document
+      .getElementById("back-to-menu-game")
+      .addEventListener("click", () => {
+        gameArea.style.display = "none";
+        menu.style.display = "flex";
+      });
+  }
+
+  // Initialiser tous les boutons
   startGame();
-  howToPlay();
+  howToPlayButton();
+  backToMenu();
 });
